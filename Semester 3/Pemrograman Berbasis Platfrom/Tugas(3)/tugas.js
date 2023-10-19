500c00316e954f5bf2886f92774c14612166e593
Aldistwn29 <aldi600051@gmail.com>
2023-10-19 23:37:16 +0700

    frist commit

---
+++ Semester 3/Pemrograman Berbasis Platfrom/Tugas(3)/tugas.js
@@ -0,0 +1,17 @@
+const restaurant = {
+    name: "Dunia Malam",
+    city: "Sukabumi",
+    favoriteDrink: "Teh Tradisonal",
+    favoriteFood: "Ikan Gurame",
+    isVegan: true
+};
+
+const restaurant2 = [
+    {
+    name: "Dunia Malam",
+    city: "Sukabumi",
+    favoriteDrink: "Teh Tradisonal",
+    favoriteFood: "Ikan Gurame",
+    isVegan: true
+    }
+];
